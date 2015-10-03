00f7ebe5f05813adec143aeaad5441193b7f8cb9
Xennis <code@xennis.org>
2015-10-03 16:24:58 +0200

    Fix edit gallery

--- src/js/smart-photo-gallery/back-end/controller/state/main/EditGalleryController.js
+++ src/js/smart-photo-gallery/back-end/controller/state/main/EditGalleryController.js
@@ -1,13 +1,19 @@
 function EditGalleryController($scope, $state, Restangular, $http) {
 	$scope.postGallery = function(item) {
-		
-		console.log(item);
-		item.save().then(function() {
+		/* item.save does not work here, since item also has a parent/depth/etc.
+		 * attribute */
+		Restangular.one('galleries', item.id).customPUT({
+			name: item.name,
+			description: item.description,
+			file: item.file
+		}).then(function() {
 			$state.go('galleries', {
 				path: $scope.path,
 				reload: true
 			});
+			$scope.adminNotice('updated', 'Saved changes');
 		}, function() {
+			$scope.adminNotice('error', 'Failed to saved changes');
 			console.warn('Save failed');
 		});
 	};
