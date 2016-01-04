1dd39332b9c9d114a6150105d7f0baa053c583cb
Xennis <code@xennis.org>
2016-01-04 22:48:41 +0100

    fix

--- src/js/smart-photo-gallery/common/config/api.js
+++ src/js/smart-photo-gallery/common/config/api.js
@@ -7,6 +7,19 @@
 function api(RestangularProvider) {
 	RestangularProvider.setBaseUrl('http://dev.miros-truckstop.de/smart-gallery/api/');
 	RestangularProvider.setFullResponse(true);
+	
+	RestangularProvider.addFullRequestInterceptor(function(element, operation, what, url, headers, params, httpConfig) {
+		if (operation === 'put' && what === 'photos') {
+			// TODO: improve (maybe use a whiteliste)
+			for (var i=0; i<element.length; i++) { 
+				delete element[i]['photographer_id'];
+				delete element[i]['photographer_name'];
+			}
+			return { 
+				element: element
+			};
+		}
+	});
 };
 	
 api.$inject = ['RestangularProvider'];
